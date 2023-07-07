843f0611085922c3ccca9e1f7b6e9cbad6f170d2
7thCoder <nuibima@gmail.com>
2023-07-07 02:57:18 +0100

    detab program in C

---
+++ program3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define tabwidth 4
+
+int main(){
+    int c;
+
+    while((c=getchar())!=EOF){
+        if(c=='\t'){
+            for(int i=0;i<tabwidth;i++){
+                putchar(' ');
+            }
+        }
+        else{
+            putchar(c);
+        }
+    }
+    return 0;
+}
